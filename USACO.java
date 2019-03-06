1cf66d2148466dd47da65fd91ac7c4300aa0aae9
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-06 10:48:45 -0500

    Comments added!

--- USACO.java
+++ USACO.java
@@ -1,13 +1,15 @@
 public class USACO{
+// These fields are for the Bronze Problem
   private int row;
   private int col;
   private int Elevation;
   private int[][] field;
+// These fields are for the Silver Problem
 
   \* This section is only for the Bronze problem *\
 
   public static void takeinputs(String filename){
-
+    
   }
 
 
