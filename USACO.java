4776795a27e5a80fd80b85b3d620a99fc07b9d47
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-06 10:46:24 -0500

    Boundaries set for Bronze and Silver problems!

--- USACO.java
+++ USACO.java
@@ -3,4 +3,23 @@ public class USACO{
   private int col;
   private int Elevation;
   private int[][] instructions;
+
+  \* This section is only for the Bronze problem *\
+
+
+
+
+
+
+  \* End of Bronze Problem *\
+
+  \* This section is only for the Silver problem *\
+
+
+
+
+
+
+  \* End of Silver Problem *\
+
 }
